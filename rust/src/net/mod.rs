83d25a504dc222354cc7cd240ddbee67f2d82569
Finn-Thorben Sell <dev@finn-thorben.me>
2022-08-06 15:10:34 +0200

    use actor based pixmap api in network request handling

--- rust/src/net/mod.rs
+++ rust/src/net/mod.rs
@@ -2,15 +2,16 @@
 //! Networking layer for pixelflut servers and clients as well as on-the-wire protocol handling
 //!
 
+use actix::Addr;
 use std::convert::TryFrom;
 
 use anyhow::Result;
 use bytes::{Buf, Bytes};
 
 use crate::net::framing::Frame;
-use crate::pixmap::{Pixmap, SharedPixmap};
-use crate::protocol::{Request, Response, StateEncodingAlgorithm};
-use crate::state_encoding::SharedMultiEncodings;
+use crate::pixmap::pixmap_actor::{GetPixelMsg, GetSizeMsg, PixmapActor, SetPixelMsg};
+use crate::pixmap::Pixmap;
+use crate::protocol::{Request, Response};
 
 pub mod framing;
 pub mod tcp_server;
@@ -20,19 +21,15 @@ pub mod ws_server;
 //static LOG_TARGET: &str = "pixelflut.net";
 
 /// handle a request frame and return a response frame
-fn handle_frame<P, B>(
-    input: Frame<B>,
-    pixmap: &SharedPixmap<P>,
-    encodings: &SharedMultiEncodings,
-) -> Option<Frame<Bytes>>
+async fn handle_frame<P, B>(input: Frame<B>, pixmap_addr: &Addr<PixmapActor<P>>) -> Option<Frame<Bytes>>
 where
-    P: Pixmap,
+    P: Pixmap + Unpin + 'static,
     B: Buf,
 {
     // try parse the received frame as request
     match Request::try_from(input) {
         Err(e) => Some(Frame::new_from_string(e.to_string())),
-        Ok(request) => match handle_request(request, pixmap, encodings) {
+        Ok(request) => match handle_request(request, pixmap_addr).await {
             Err(e) => Some(Frame::new_from_string(e.to_string())),
             Ok(response) => response.map(|r| r.into()),
         },
@@ -40,31 +37,40 @@ where
 }
 
 /// handle a request and return a response
-fn handle_request<P>(
-    request: Request,
-    pixmap: &SharedPixmap<P>,
-    encodings: &SharedMultiEncodings,
-) -> Result<Option<Response>>
+async fn handle_request<P>(request: Request, pixmap_addr: &Addr<PixmapActor<P>>) -> Result<Option<Response>>
 where
-    P: Pixmap,
+    P: Pixmap + Unpin + 'static,
 {
+    let pixmap_size = pixmap_addr.send(GetSizeMsg {}).await??;
+
     match request {
-        Request::Size => Ok(Some(Response::Size(pixmap.get_size()?.0, pixmap.get_size()?.1))),
+        Request::Size => Ok(Some(Response::Size(pixmap_size.0, pixmap_size.1))),
         Request::Help(topic) => Ok(Some(Response::Help(topic))),
-        Request::PxGet(x, y) => Ok(Some(Response::Px(x, y, pixmap.get_pixel(x, y)?))),
+        Request::PxGet(x, y) => Ok(Some(Response::Px(
+            x,
+            y,
+            pixmap_addr.send(GetPixelMsg { x: x, y: y }).await??,
+        ))),
         Request::PxSet(x, y, color) => {
-            pixmap.set_pixel(x, y, color)?;
+            pixmap_addr
+                .send(SetPixelMsg {
+                    x: x,
+                    y: y,
+                    color: color,
+                })
+                .await??;
             Ok(None)
         }
-        Request::State(algorithm) => match algorithm {
-            StateEncodingAlgorithm::Rgb64 => Ok(Some(Response::State(
-                algorithm,
-                encodings.rgb64.lock().unwrap().clone(),
-            ))),
-            StateEncodingAlgorithm::Rgba64 => Ok(Some(Response::State(
-                algorithm,
-                encodings.rgba64.lock().unwrap().clone(),
-            ))),
-        },
+        // Request::State(algorithm) => match algorithm {
+        //     StateEncodingAlgorithm::Rgb64 => Ok(Some(Response::State(
+        //         algorithm,
+        //         encodings.rgb64.lock().unwrap().clone(),
+        //     ))),
+        //     StateEncodingAlgorithm::Rgba64 => Ok(Some(Response::State(
+        //         algorithm,
+        //         encodings.rgba64.lock().unwrap().clone(),
+        //     ))),
+        // },
+        _ => todo!("Re-implement encodings"),
     }
 }
